72e494dd175d9f5774ecd82bb5728fa3a1668b4e
amjadjirawi <amjadjearawy@gmail.com>
2020-07-27 22:07:11 +0300

    litle updates

--- js/books.js
+++ js/books.js
@@ -2,28 +2,17 @@
 
 
 function MostRecentcat() {
-    let MostRecentarr = allBooks;
-    MostRecentarr.sort(function (a, b) {
-        return a.publishedYear - b.publishedYear;
-      });
-    MostRecentarr.reverse();
+  let MostRecentarr = allBooks;
+  MostRecentarr.sort(function (a, b) {
+    return a.publishedYear - b.publishedYear;
+  });
+  MostRecentarr.reverse();
 
-
-    let MostRecentbooks = document.querySelectorAll(".most-recent img");
-     for (let index=0; index <MostRecentbooks.length; index++){
-        MostRecentbooks[index].src= MostRecentarr[index].cover;
-        MostRecentbooks[index].alt= MostRecentarr[index].title;
-console.log()
-     }
-
- 
-        
+  let MostRecentbooks = document.querySelectorAll(".most-recent img");
+  for (let index = 0; index < MostRecentbooks.length; index++) {
+    MostRecentbooks[index].src = MostRecentarr[index].cover;
+    MostRecentbooks[index].alt = MostRecentarr[index].title;
     
-
-
-
-
+  }
 }
-
-
 MostRecentcat();
