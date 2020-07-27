a2089946b0af8ce36cefbfb308dabbbf7d084480
MarahIrshaid <marah.irshaid96@gmail.com>
2020-07-27 10:38:04 +0300

    marah's objects

--- js/app.js
+++ js/app.js
@@ -43,7 +43,29 @@ new BooksGenerator(
   "Romance",
   "Austen’s timeless romantic classic, follows the lives of the five Bennett sisters, who live in a time where an advantageous marriage and social status are considered a fundamental for any woman to stand a fair chance at life. Set at the turn of the 19th century, Pride and Prejudice catches a perfect glimpse not only of a time when women were socially and economically dependent solely on their marital status, but also as an age of enlightenment and witness of the French Revolution."
 );
+new BooksGenerator(
+  "alice's adventures in wonderland",
+  1865,
+  "Lewis Carroll ",
+  "fantasy",
+  " An acclaimed children’s classic depicting the odd, but riveting journeys of the curiousAlice as she explores the surreal world of Wonderland. Written by Charles Lutwidge Dodgsonor better known under his pseudonym Lewis Caroll, this episodic novel is assembled in twelvechapters each containing a prominent adventure. The departure from logic and its embracementof pure imagination is what makes Alice’s Adventures in Wonderland a model for fantasy novels and a timeless classic."
+);
+new BooksGenerator(
+  "tarzan of the apes",
+  1912,
+  "Edgar Rice Burroughs ",
+  "fantasy",
+  " An aristocratic English family is marooned off the coast of West Africa. They find their way into the interior of the dense jungle that lines the coast and here, Lord Greystoke is killed by a predatory ape. Lady Greystoke survives with her infant boy, but in a few months, she too succumbs to the perils of jungle life. The baby is adopted by a maternal she-ape who nurses him along with her own child. This marks the dawn of a legend – Tarzan of the Apes."
+);
+new BooksGenerator(
+  "the man who was thursday",
+  1908,
+  " G. K. Chesterton ",
+  "fantasy",
+  "Two poets in a London park at sunset, debating on the attributes of poetry and whether it's really a metaphor for anarchy. A group that meets in secret, planning to overthrow the world order. Disguises and deceptions, ideals and ideology. A medley of themes and genres makes this a great read for anyone who's a fan of Chesterton and his iconic Father Brown."
+ );
 
+ 
 const books = [
   "jane eyre",
   "pride and prejudice",
